cf5ff3988cb5a7119f30f8f20b0fa57717c3044c
Alfred <spicestar@libero.it>
2016-09-20 10:02:10 +0200

    Fix Windows Universal

--- src/windows/ReadKeyProxy.js
+++ src/windows/ReadKeyProxy.js
@@ -1,7 +1,12 @@
-cordova.commandProxy.add("readkey",{
-    read:function(successCallback,errorCallback,strInput) {
-			var key = "matteo123";
-          successCallback(key);
-        
+var cordova = require('cordova'),
+    readkey= require('./readkey');
+
+module.exports = {
+
+    read: function (successCallback, errorCallback, strInput) {
+		var key = "matteo123";
+        successCallback(key);
     }
-});
+};
+
+require("cordova/exec/proxy").add("readkey", module.exports);
