dd449c0c06d1e3bdb14bc1c1d332cef7c7005ee7
Alfred <spicestar@libero.it>
2016-09-20 12:06:16 +0200

    Fix

--- src/windows/ReadKeyProxy.js
+++ src/windows/ReadKeyProxy.js
@@ -1,11 +1,8 @@
-var readkey= require('./readkey');
-
 module.exports = {
-
-    read: function (successCallback, errorCallback, strInput) {
-		var key = "matteo123";
-        successCallback(key);
-    }
+        read: function (successCallback, errorCallback, strInput) {
+			var key = "matteo123";
+			successCallback(key);
+		}
 };
 
 require("cordova/exec/proxy").add("readkey", module.exports);
